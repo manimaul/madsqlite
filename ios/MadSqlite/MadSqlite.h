7d0907bc8fbe30a5740cea1f8dbb5381cb5dc025
William Kamp <manimaul@gmail.com>
2016-11-25 13:50:37 -0800

    objective-c docs

--- ios/MadSqlite/MadSqlite.h
+++ ios/MadSqlite/MadSqlite.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 William Kamp. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MadSqlite.
 FOUNDATION_EXPORT double MadSqliteVersionNumber;
